b539cedf6041d5e43b13af2510335b30a25e2e9c
cloudsnight10693 <cloudsnight10693@gmail.com>
2019-11-18 09:48:58 +0700

    1. Setelah membuat seeder-nya jangan lupa eksekusi pada terminal:   - composer dump-autoload Supaya db:seed dapat bekerja. 2. Eksekusi db:seed dapat didaftarkan pemanggilan kelas seedernya di- DatabaseSeeder.php
    On branch seeding
    Changes to be committed:
    	new file:   database/seeds/BlogsTableSeeder.php
    	modified:   database/seeds/DatabaseSeeder.php

---
+++ database/seeds/BlogsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class BlogsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+   public function run()
+   {
+      $faker = Faker::create();
+
+      for ($i=0; $i < 5; $i++) { 
+         DB::table('blogs')->insert([
+            'title' => $faker->sentence(rand(2, 4)),
+            'description' => $faker->text(200)
+         ]);
+      }
+
+   }
+}
